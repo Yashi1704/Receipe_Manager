05116d9dada690e48cbf07c7fd14f2c1f7e45baa
Yashica Bhardwaj <yashicabhardwaj6@gmail.com>
2025-07-04 20:33:36 +0530

    Innitial comment

---
+++ Backend/routes/auth.js
@@ -0,0 +1,77 @@
+import express from "express";
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+import { protect } from "../middleware/auth.js";
+
+const router = express.Router();
+
+// Register a user
+router.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "Please fill all fields" });
+    }
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    const user = await User.create({ username, email, password });
+    const token = generateToken(user._id);
+    res.status(201).json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      token,
+    });
+  }  catch (err) {
+  if (err.code === 11000) {
+    // Duplicate key error
+    return res.status(400).json({
+      message: "User with this email or username already exists",
+      duplicateKey: err.keyValue,
+    });
+  }
+
+  console.error("❌ Registration error:", err.message);
+  res.status(500).json({ message: "Server error", error: err.message });
+}
+
+});
+
+// Login
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user || !(await user.matchPassword(password))) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+    const token = generateToken(user._id);
+    res.json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      token,
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+router.get("/me", protect, async (req, res) => {
+  res.status(200).json(req.user);
+});
+
+// Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
+};
+
+export default router;
